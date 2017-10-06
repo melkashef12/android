35f848bcff1bac5eb6baefec0ff01eb85b7ce4e4
Melkashef <melkashef@e-biz.fr>
2017-10-06 13:13:38 +0200

    Init

---
+++ hfad/Chapter-2-intent/app/src/main/java/mo/hfad/com/chapter_2_intent/CreateMessageActivity.java
@@ -0,0 +1,12 @@
+package mo.hfad.com.chapter_2_intent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_create_message);
+  }
+}
